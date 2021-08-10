07f9091ede9032e70e7756b216a564ec4f7ef226
Jorge Blanco <jbsfreelance@gmail.com>
2021-08-10 07:15:04 -0700

    Basic unit spawner logic

--- Assets/Scripts/RTS/Networking/NetworkManagerBasics.cs
+++ Assets/Scripts/RTS/Networking/NetworkManagerBasics.cs
@@ -5,6 +5,8 @@ namespace RTS.Networking
 {
     public class NetworkManagerBasics : NetworkManager
     {
+        [SerializeField] private GameObject unitSpawnerPrefab;
+        
         public override void OnServerAddPlayer(NetworkConnection conn)
         {
             base.OnServerAddPlayer(conn);
@@ -12,6 +14,15 @@ namespace RTS.Networking
             var player = conn.identity.GetComponent<NetworkedPlayerData>();
             player.SetDisplayName($"Player {numPlayers}");
             player.SetDisplayColor(new Color(Random.value, Random.value, Random.value));
+
+            var playerTransform = conn.identity.transform;
+            var unitSpawnerInstance = Instantiate(
+                unitSpawnerPrefab,
+                playerTransform.position,
+                playerTransform.rotation
+            );
+            
+            NetworkServer.Spawn(unitSpawnerInstance, conn);
         }
     }
 }
